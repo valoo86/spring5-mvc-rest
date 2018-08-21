d3787af4e7e9e858040c8cf3c3ab0a0d6d586f52
Valentin Matignon <valentinmatignon@gmail.com>
2018-08-21 14:33:39 +0200

    Added categories in bootstrap

---
+++ src/main/java/guru/springfamework/bootstrap/Bootstrap.java
@@ -0,0 +1,42 @@
+package guru.springfamework.bootstrap;
+
+import guru.springfamework.domain.Category;
+import guru.springfamework.repositories.CategoryRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Bootstrap implements CommandLineRunner {
+
+    private final CategoryRepository categoryRepository;
+
+    public Bootstrap(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Category fruits = new Category();
+        fruits.setName("Fruits");
+
+        Category dried = new Category();
+        fruits.setName("Dried");
+
+        Category fresh = new Category();
+        fruits.setName("Fresh");
+
+        Category exotic = new Category();
+        fruits.setName("Exotic");
+
+        Category nuts = new Category();
+        fruits.setName("nuts");
+
+        categoryRepository.save(fruits);
+        categoryRepository.save(dried);
+        categoryRepository.save(fresh);
+        categoryRepository.save(exotic);
+        categoryRepository.save(nuts);
+
+        System.out.println("Data Loadaed = " + categoryRepository.count());
+    }
+}
